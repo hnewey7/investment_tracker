8db80237b433a18faeebb3cd3c8249f69ed26eee
hnewey7 <hnewey7@gmail.com>
2025-06-30 12:34:39 +0100

    feat: add endpoints for get individual user.
    - add /users/user endpoint for individual user.
    - test for all current endpoints.
    - test for valid /users/user endpoint.

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -35,11 +35,9 @@ def get_users(*, session: SessionDep, skip: int=0, limit: int=100):
     """
     count_statement = select(func.count()).select_from(User)
     count = session.exec(count_statement).one()
-    print(count)
 
     statement = select(User).offset(skip).limit(limit)
     users = session.exec(statement).all()
-    print(users)
 
     return UsersPublic(data=users, count=count)
 
@@ -67,4 +65,63 @@ def create_user(*, session: SessionDep, user_in: UserCreate) -> Any:
         )
 
     user = crud.create_user(session=session, user_create=user_in)
+    return user
+
+
+@router.get(
+    "/{user_id}",
+    response_model=UserPublic
+)
+def get_user_by_id(*, session: SessionDep, user_id: int):
+    """
+    Get user by id.
+
+    Args:
+        session (SessionDep): SQL session.
+        user_id (int): User ID.
+    """
+    user = crud.get_user_by_id(session=session, id=user_id)
+    if not user:
+        raise HTTPException(
+            status_code=400,
+            detail="No user exists with this id."
+        )
+    return user
+
+
+@router.get(
+    "/user/",
+    response_model=UserPublic
+)
+def get_user(*, session: SessionDep, username: str = None, email: str = None):
+    """
+    Get individual user by username or email.
+
+    Args:
+        session (SessionDep): SQL session.
+        username (str, optional): Username. Defaults to None.
+        email (str, optional): Email. Defaults to None.
+    """
+    if not username and not email:
+        raise HTTPException(
+            status_code=400,
+            detail="No username or email address provided to get user."
+        )
+    elif email:
+        user = crud.get_user_by_email(session=session, email=email)
+        
+        # Check details match username if provided.
+        if username and user.username != username:
+            raise HTTPException(
+                status_code=400,
+                detail="Username provided doesn't match username for email provided."
+            )
+    else:
+        user = crud.get_user_by_username(session=session, username=username)
+
+    if not user:
+        raise HTTPException(
+            status_code=400,
+            detail="No user exists with these details."
+        )
     return user
---
+++ app/tests/test_users_route.py
@@ -0,0 +1,161 @@
+'''
+Module for testing the users route.
+
+Created on 29-06-2025
+@author: Harry New
+
+'''
+
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from app.models import User
+from app.tests.utils.utils import random_email, random_lower_string
+
+# - - - - - - - - - - - - - - - - - - -
+
+def test_get_users(client: TestClient, user: User):
+    """
+    Test get users endpoint.
+
+    Args:
+        client (TestClient): Test client.
+        db (Session): SQL session.
+    """
+    # Send request to get users.
+    response = client.get("/users")
+    user_list = response.json()
+
+    # Check length of response.
+    assert len(user_list["data"]) == 1
+    assert user_list["count"] == 1
+
+    # Check user provided.
+    assert user_list["data"][0]["username"] == user.username
+    assert user_list["data"][0]["email"] == user.email
+    assert user_list["data"][0]["id"] == user.id
+
+
+def test_create_user(client: TestClient):
+    """
+    Test creating user endpoint.
+
+    Args:
+        client (TestClient): TestClient.
+    """
+    # User properties.
+    user_properties = {
+        "username": random_lower_string(),
+        "email": random_email(),
+        "password": random_lower_string()
+    }
+
+    # Send request to create user.
+    response = client.post("/users",json=user_properties)
+    user_response = response.json()
+    
+    # Check valid properties.
+    valid_keys = ["username","email","id"]
+    for key in valid_keys:
+        assert key in user_response
+
+    # Check properties.
+    assert user_response["username"] == user_properties["username"]
+    assert user_response["email"] == user_properties["email"]
+
+
+def test_create_existing_user(client: TestClient, user: User):
+    """
+    Tets creating user with existing user details.
+
+    Args:
+        client (TestClient): Test client.
+        user (User): Test user.
+    """
+    # Get user properties from existing user.
+    user_properties = {
+        "username": user.username,
+        "email": user.email,
+        "password": random_lower_string()
+    }
+
+    # Send request to create user.
+    response = client.post("/users",json=user_properties)
+    assert response.status_code == 400
+
+
+def test_get_user_by_id(client: TestClient, user:User):
+    """
+    Test get user by id endpoint.
+
+    Args:
+        client (TestClient): Test client.
+        user (User): Test user.
+    """
+    # Send get request for specific user id.
+    response = client.get(f"/users/{user.id}")
+    get_user_response = response.json()
+
+    # Check two responses are equal.
+    assert get_user_response["username"] == user.username
+    assert get_user_response["email"] == user.email
+    assert get_user_response["id"] == user.id
+
+
+def test_get_user_by_id_invalid(client: TestClient):
+    """
+    Test get user by id with invalid id.
+
+    Args:
+        client (TestClient): Test client.
+    """
+    # Send get request for invalid id.
+    response = client.get(f"/users/1")
+    assert response.status_code == 400
+
+
+def test_get_user(client: TestClient, user: User):
+    """
+    Test get user endpoint.
+
+    Args:
+        client (TestClient): Test client.
+        user (User): Test user.
+    """
+    # Get user by username.
+    response = client.get("/users/user/",params={
+        "username":user.username
+    })
+    username_response = response.json()
+    assert response.status_code == 200
+    assert username_response["username"] == user.username
+    assert username_response["email"] == user.email
+    assert username_response["id"] == user.id
+
+    # Get user by email.
+    response = client.get("/users/user/",params={
+        "email":user.email
+    })
+    email_response = response.json()
+    assert response.status_code == 200
+    assert email_response["username"] == user.username
+    assert email_response["email"] == user.email
+    assert email_response["id"] == user.id
+
+    # Get user with email and username
+    response = client.get("/users/user/",params={
+        "email":user.email,
+        "username":user.username
+    })
+    email_username_response = response.json()
+    assert response.status_code == 200
+    assert email_username_response["username"] == user.username
+    assert email_username_response["email"] == user.email
+    assert email_username_response["id"] == user.id
+
+    # Get user with valid email and invalid username.
+    response = client.get("/users/user/",params={
+        "email":user.email,
+        "username":random_lower_string()
+    })
+    assert response.status_code == 400
